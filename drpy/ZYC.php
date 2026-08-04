8f21ce2538e85671d95bc9c94924fcc35e5d6faa
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-04 08:18:38 +0800

    000

--- drpy/ZYC.php
+++ drpy/ZYC.php
@@ -1,14 +1,14 @@
 {"storeHouse": [
 {
     "sourceName": "①🍱稳定仓🍭长期有效🍭",
-    "sourceUrl": "https://fastly.jsdelivr.net/gh/LIUJEI/TB@master/drpy/AB.php"
+    "sourceUrl": "https://fastly.jsdelivr.net/gh/LIUJEI/TS@master/drpy/AB.php"
 },
 {
     "sourceName": "②🎎测试仓🍭短期有效🍭",
-    "sourceUrl": "https://fastly.jsdelivr.net/gh/LIUJEI/TB@master/drpy/AC.php"
+    "sourceUrl": "https://fastly.jsdelivr.net/gh/LIUJEI/TS@master/drpy/AC.php"
 },
 {
     "sourceName": "③🎎网友仓🍭短期有效🍭",
-    "sourceUrl": "https://fastly.jsdelivr.net/gh/LIUJEI/TB@master/drpy/CC.php"
+    "sourceUrl": "https://fastly.jsdelivr.net/gh/LIUJEI/TS@master/drpy/CC.php"
 }
 ]}
