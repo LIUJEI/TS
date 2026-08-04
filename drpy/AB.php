c5488d3138ad48fb5f8eba284bcc716d0bc2a85c
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-04 08:31:43 +0800

    000
    Updated URLs in AB.php to point to the main branch.

--- drpy/AB.php
+++ drpy/AB.php
@@ -2,15 +2,15 @@
 "urls": [
 {
     "name": "🚸《影视》",
-    "url": "https://raw.githubusercontent.com/LIUJEI/TS/master/影视.php"
+    "url": "https://raw.githubusercontent.com/LIUJEI/TS/main/影视.php"
 },
 {
     "name": "🚸《魔盒》",
-    "url": "https://raw.githubusercontent.com/LIUJEI/TS/master/魔盒.php"
+    "url": "https://raw.githubusercontent.com/LIUJEI/TS/main/魔盒.php"
 },
 {
     "name": "🚸《休闲》",
-    "url": "https://raw.githubusercontent.com/LIUJEI/TS/master/休闲.php"
+    "url": "https://raw.githubusercontent.com/LIUJEI/TS/main/休闲.php"
 }
 ]
 }
