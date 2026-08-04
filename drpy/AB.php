056f0985fd00dfe3f7a5cb05d7742572670aaa79
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-04 08:26:30 +0800

    000
    Updated URLs in AB.php to point to the TS repository.

--- drpy/AB.php
+++ drpy/AB.php
@@ -2,15 +2,15 @@
 "urls": [
 {
     "name": "🚸《影视》",
-    "url": "https://raw.githubusercontent.com/LIUJEI/TB/master/影视.php"
+    "url": "https://raw.githubusercontent.com/LIUJEI/TS/master/影视.php"
 },
 {
     "name": "🚸《魔盒》",
-    "url": "https://raw.githubusercontent.com/LIUJEI/TB/master/魔盒.php"
+    "url": "https://raw.githubusercontent.com/LIUJEI/TS/master/魔盒.php"
 },
 {
     "name": "🚸《休闲》",
-    "url": "https://raw.githubusercontent.com/LIUJEI/TB/master/休闲.php"
+    "url": "https://raw.githubusercontent.com/LIUJEI/TS/master/休闲.php"
 }
 ]
 }
