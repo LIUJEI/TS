ac32cc15d9ef7826ecee0e46f443881384f6794a
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-04 09:03:36 +0800

    000
    Updated URLs to remove unnecessary '@' character in paths.

--- drpy/AB.php
+++ drpy/AB.php
@@ -2,15 +2,15 @@
 "urls": [
 {
     "name": "🚸《影视》",
-    "url": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@/main/MH.php"
+    "url": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@main/MH.php"
 },
 {
     "name": "🚸《魔盒》",
-    "url": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@/main/MX.php"
+    "url": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@main/MX.php"
 },
 {
     "name": "🚸《休闲》",
-    "url": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@/main/XX.php"
+    "url": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@main/XX.php"
 }
 ]
 }
