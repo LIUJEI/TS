60fdf236f0ed52769b02cf0079cefa51664b9d08
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-07 04:02:44 +0800

    Update ZYC.php

--- drpy/ZYC.php
+++ drpy/ZYC.php
@@ -1,14 +1,14 @@
 {"storeHouse": [
 {
     "sourceName": "①🍱稳定仓🍭长期有效🍭",
-    "sourceUrl": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@main/drpy/AB.php"
+    "sourceUrl": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/drpy/AB.php"
 },
 {
     "sourceName": "②🎎测试仓🍭短期有效🍭",
-    "sourceUrl": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@main/drpy/AC.php"
+    "sourceUrl": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/drpy/AC.php"
 },
 {
     "sourceName": "③🎎网友仓🍭短期有效🍭",
-    "sourceUrl": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@main/drpy/CC.php"
+    "sourceUrl": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/drpy/CC.php"
 }
 ]}
