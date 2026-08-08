e0d077d83ea77d3c1dd5cce7644de96c70989384
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-08 14:39:47 +0800

    Update AB.php

--- drpy/AB.php
+++ drpy/AB.php
@@ -5,6 +5,6 @@
 {"name": "🚸《魔盒》","url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MX.php"},
 {"name": "🚸《饭佬》","url": "http://www.饭太硬.cc/tv"},
 {"name": "🚸《张佬》","url": "https://zhangqun1818.serv00.net/zq/api.json"},
-{"name": "🚸《集多》VOX","url": "http://rihou.cc:88/demo.php"}
+{"name": "🚸《集多》","url": "http://rihou.cc:88/demo.php"}
 ]
 }
