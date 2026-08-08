cf3dac04e96db4c847b069243b5495a2a18b0bf6
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-08 14:34:45 +0800

    Update AB.php

--- drpy/AB.php
+++ drpy/AB.php
@@ -1,16 +1,10 @@
 {
 "urls": [
-{
-    "name": "🚸《影视》",
-    "url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MH.php"
-},
-{
-    "name": "🚸《备用》",
-    "url": "https://cnb.cool/my-liuyun/TVbox/-/git/raw/main/MG.php"
-},
-{
-    "name": "🚸《魔盒》",
-    "url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MX.php"
-}
+{"name": "🚸《自用》","url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MH.php"},
+{"name": "🚸《备用》","url": "https://cnb.cool/my-liuyun/TVbox/-/git/raw/main/MG.php"},
+{"name": "🚸《魔盒》","url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MX.php"},
+{"name": "🚸《饭佬》","url": "http://www.饭太硬.cc/tv"},
+{"name": "🚸《张佬》","url": "https://zhangqun1818.serv00.net/zq/api.json"},
+{"name": "🚸《集多》VOX","url": "http://rihou.cc:88/demo.php"},
 ]
 }
