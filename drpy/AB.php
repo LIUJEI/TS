0b99a8ba821908ef2af271fe2f4746bb2c25ecc5
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-08 08:57:35 +0800

    Update AB.php

--- drpy/AB.php
+++ drpy/AB.php
@@ -2,7 +2,11 @@
 "urls": [
 {
     "name": "🚸《影视》",
-    "url": "https://cnb.cool/my-liuyun/TVbox/-/git/raw/main/MH.php"
+    "url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MX.php"
+},
+{
+    "name": "🚸《备用》",
+    "url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MX.php"
 },
 {
     "name": "🚸《魔盒》",
