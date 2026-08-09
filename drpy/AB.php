98d306aaf45a80b6160312b5f554bc482d80b5ff
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-09 10:12:03 +0800

    Format JSON structure in AB.php

--- drpy/AB.php
+++ drpy/AB.php
@@ -2,7 +2,7 @@
 "urls": [
 {"name": "🚸《自用》","url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MH.php"},
 {"name": "🚸《备用》","url": "https://cnb.cool/my-liuyun/TVbox/-/git/raw/main/MG.php"},
-{"name": "🚸《魔盒》","url": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@main/main/MX.php"},
+{"name": "🚸《魔盒》","url": "https://testingcf.jsdelivr.net/gh/LIUJEI/TS@main/MX.php"},
 {"name": "🚸《饭佬》","url": "http://www.饭太硬.cc/tv"},
 {"name": "🚸《集多》","url": "http://rihou.cc:88/demo.php"}
 ]
