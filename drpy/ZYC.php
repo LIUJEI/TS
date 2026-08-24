ea86a2d1c03a1e67cd1a0c172450c77d1e758c69
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-24 16:45:01 +0800

    Update ZYC.php

--- drpy/ZYC.php
+++ drpy/ZYC.php
@@ -1,7 +1,7 @@
 {"storeHouse": [
 {
     "sourceName": "①🍱稳定仓🍭长期有效🍭",
-    "sourceUrl": "https://gcore.jsdelivr.net/gh/LIUJEI/TS@main/drpy/AB.php"
+    "sourceUrl": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/drpy/AB.php"
 },
 {
     "sourceName": "②🎎测试仓🍭短期有效🍭",
