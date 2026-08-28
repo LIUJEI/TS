c18597d5f49c64b23202ffd36cd2252a76a0a439
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-08-28 08:25:50 +0800

    Update AB.php

--- drpy/AB.php
+++ drpy/AB.php
@@ -2,8 +2,8 @@
 "urls": [
 {"name": "🚸《自用》","url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MH.php"},
 {"name": "🚸《备用》","url": "https://cnb.cool/my-liuyun/TVbox/-/git/raw/main/MF.php"},
-{"name": "🚸《备用》","url": "https://cnb.cool/my-liuyun/TVbox/-/git/raw/main/MG.php"},
 {"name": "🚸《魔盒》","url": "https://gh-proxy.com/https://raw.githubusercontent.com/LIUJEI/TS/main/MX.php"},
+{"name": "🚸《夏夏》","url": "https://11405.kstore.space/xiaye/qk4k.json"},
 {"name": "🚸《饭佬》","url": "http://www.饭太硬.cc/tv"}
 ]
 }
