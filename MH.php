1bb8df6e818378fe1d1527f850c0348cc6ed99ab
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-09-01 15:06:44 +0800

    Update MH.php

--- MH.php
+++ MH.php
@@ -1,6 +1,6 @@
 {
   /*-----------------------JAR包接口&壁纸接口(可替换)-----------------------*/
-  "spider": "./JAR/MS.jar;md5;8aa48c5533a2a6d343060c7044614fae",
+  "spider": "https://gitee.com/lyun_1_0/TS/raw/main/JAR/spider.jar;md5;565bb39190f304bfd73a52e986235de3",
   "wallpaper": "http://127.0.0.1:9978/proxy?do=wallpaper",
   "logo": "https://pic7.fukit.cn/autoupload/gE6Y0Af2tjXBCNig6CtNDI12_FRYNb81z6UPhMWD8iI/20251224/dJkT/1080X1080/logo.jpg/webp",
   "danmaku": "http://127.0.0.1:9978/proxy?do=danmaku&name={name}&episode={episode}",
