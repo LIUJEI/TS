d1c460ef121ad30a47d55c8e2a02f180e236b12b
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-09-02 05:38:55 +0800

    Update MH.php

--- MH.php
+++ MH.php
@@ -1,6 +1,6 @@
 {
   /*-----------------------JAR包接口&壁纸接口(可替换)-----------------------*/
-  "spider": "https://gitee.com/lyun_1_0/TS/raw/main/JAR/MS1.jar;md5;0703a4957b055c239ec0623b105a4bd5",
+  "spider": "https://gitee.com/lyun_1_0/TS/raw/main/JAR/MS_planB.jar;md5;0703a4957b055c239ec0623b105a4bd5",
   "wallpaper": "http://127.0.0.1:9978/proxy?do=wallpaper",
   "logo": "https://pic7.fukit.cn/autoupload/gE6Y0Af2tjXBCNig6CtNDI12_FRYNb81z6UPhMWD8iI/20251224/dJkT/1080X1080/logo.jpg/webp",
   "danmaku": "http://127.0.0.1:9978/proxy?do=danmaku&name={name}&episode={episode}",
@@ -15,7 +15,7 @@
 },
 {
     "key": "csp_Notice",
-    "name": "🎬┣公告┫09.01",
+    "name": "🎬┣公告┫09.02",
     "type": 3,
     "api": "csp_Notice",
     "jar": "https://gitee.com/lyun_1_0/tb/raw/master/liuli.php",
