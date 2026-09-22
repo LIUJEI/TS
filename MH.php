46184cad6ebcd61ea7b49c90ce8f2f4607ca7373
LIUJEI <155596385+LIUJEI@users.noreply.github.com>
2026-09-22 09:07:32 +0800

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- MH.php
+++ MH.php
@@ -1,6 +1,6 @@
 {
   /*-----------------------JAR包接口&壁纸接口(可替换)-----------------------*/
-  "spider": "https://gitee.com/lyun_1_0/TS/raw/main/JAR/spider.jar;md5;da7b873a55d0e8cbadc7dfac14614650",
+  "spider": "https://8815.kstore.vip/tvbox/9527.jar;md5;da7b873a55d0e8cbadc7dfac14614650",
   "wallpaper": "http://127.0.0.1:9978/proxy?do=wallpaper",
   "logo": "https://pic7.fukit.cn/autoupload/gE6Y0Af2tjXBCNig6CtNDI12_FRYNb81z6UPhMWD8iI/20251224/dJkT/1080X1080/logo.jpg/webp",
   "danmaku": "http://127.0.0.1:9978/proxy?do=danmaku&name={name}&episode={episode}",
@@ -23,7 +23,7 @@
 },
  {
       "key": "搜剧AI",
-      "name": "🎈看剧AI丨蓝光秒播",
+      "name": "🎈看剧AI丨秒播",
       "api": "csp_SouJu",
       "type": 3,
       "searchable": 1,
